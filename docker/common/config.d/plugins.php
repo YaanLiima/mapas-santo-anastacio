9ffc3e76df9a59f2e5ed8cb47575a78506675531
Yan_Liima <45018729+YaanLiima@users.noreply.github.com>
2026-01-11 17:53:12 -0300

    .
    .

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -3,8 +3,52 @@
 return [
     'plugins' => [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
-        'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        'Accessibility',
         'SpamDetector',
-		'Accessibility',
-    ]
+        'RecreatePCacheOnLogin',
+        'Analytics',
+        'Zammad' => [
+            'namespace' => 'Zammad',
+            'config' => [
+                'enabled' => false,
+                'url' => 'https://seu-servidor-zammad.com/assets/chat/chat.min.js',
+                'background' => '#056614',
+                'title' => 'Dúvidas? Fale conosco',
+                'chatId' => 1,
+                'instalacao' => 'cultura.santoanastacio.sp.gov.br',
+                'estado' => 'SP',
+            ],
+        ],
+
+        // ===== Aldir Blanc =====
+        'AldirBlanc' => [
+            'namespace' => 'AldirBlanc',
+            'config' => [
+                'homologacao_requer_validacao' => [],
+                'consolidacao_requer_validacao' => ['dataprev', 'scge'],
+            ],
+        ],
+
+        'AldirBlancDataprev' => [
+            'namespace' => 'AldirBlancDataprev',
+            'config' => [
+                'exportador_requer_homologacao' => true,
+                'consolidacao_requer_validacoes' => ['scge'],
+            ],
+        ],
+
+        'SCGE' => [
+            'namespace' => 'AldirBlancValidador',
+            'config' => [
+                'slug' => 'scge',
+                'name' => 'SCGE',
+                'exportador_requer_homologacao' => true,
+                'exportador_requer_validacao' => [],
+                'consolidacao_requer_validacoes' => ['dataprev'],
+                'inciso1' => [
+                    'CPF' => 33,
+                ],
+            ],
+        ],
+    ],
 ];
